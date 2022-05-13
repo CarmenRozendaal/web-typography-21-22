960d7cfa57ba13d268dd2b2057bc796414f75c95
CarmenRozendaal <56074362+CarmenRozendaal@users.noreply.github.com>
2022-05-13 18:06:15 +0200

    Add files via upload

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,12 +1,11 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
+	0, // beginmuziek
+	9,	// Alarm 1
+	12, // Hight Noice
+	16.5, // Alarm 2
+	23, // Badguy talks
 	25.8,
 	31,
 	32,
